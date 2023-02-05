ef32113edf41ab68baf8a9c4e744fc01d9773631
Giovanni Soulk Benedetti <giovannisoulkbenedetti@gmail.com>
2023-02-05 20:47:30 +0100

    javascript don't touch me test

--- horizzontal-web-site/horizzontal.js
+++ horizzontal-web-site/horizzontal.js
@@ -6,3 +6,29 @@ container.addEventListener('wheel', (e)=> {
     container.scrollLeft += e.deltaY;
     scrollLine.style.width = container.scrollLeft / 2 + 'px';
 });
+
+function openNav(){
+    document.getElementsByClassName('side-nav')[0].style.width = "15em";
+    document.getElementById('nav').style.opacity = "1";
+}
+
+function closeNav(){
+    document.getElementsByClassName('side-nav')[0].style.width = "5em";
+    document.getElementById('nav').style.opacity = "0";
+}
+
+
+function run(){
+    let run = document.getElementsByClassName('run')[0];
+    let randomNumber = (Math.random() * 50);
+    
+    if (randomNumber > 10) {
+        randomPosition = randomNumber +'em';
+    } else {
+        randomPosition += 10;
+    }
+
+    run.style.left = randomPosition;
+    run.style.top = randomPosition;
+    run.style.transition = "2s ease";
+}
