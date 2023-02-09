2d7675f37e757d035cca123ba85900696bc230ad
Giovanni Soulk Benedetti <giovannisoulkbenedetti@gmail.com>
2023-02-09 12:07:43 +0100

    modal

--- general-code/main.js
+++ general-code/main.js
@@ -7,3 +7,19 @@ buttonElement.addEventListener('click', () => {
   toggleHiddenElement(pElement);
   changeToFunkyColor(buttonElement);
 });
+
+/* Modal */
+
+const modalContainer = document.getElementById('modal-container');
+const closeButtonModal = document.getElementById('close-btn-modal');
+const image = document.getElementById('modal-img');
+const modalImage = document.getElementById('modal-img-01');
+
+image.onclick = function () {
+  modalContainer.style.display = "block";
+  modalImage.src = this.src;
+};
+
+closeButtonModal.onclick = function() {
+  modalContainer.style.display = 'none';
+}
